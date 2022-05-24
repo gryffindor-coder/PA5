82d85bf05f66e21c54d714fced830a13b705f92f
Jaco Malan <jacomalan.12@gmail.com>
2022-05-24 13:33:06 +0200

    Created PHP Composer Project

---
+++ code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5a10af4bb373b02000b9ac456b111a07954495c8',
+        'name' => 'enthusiastic_students/pa5',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'enthusiastic_students/pa5' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5a10af4bb373b02000b9ac456b111a07954495c8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
