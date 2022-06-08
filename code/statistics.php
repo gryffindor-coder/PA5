637d20cc26686e61664f161f8be431ce3d5660f5
gryffindor-coder <65118195+gryffindor-coder@users.noreply.github.com>
2022-06-08 03:00:54 +0200

    More additions to statistics

--- code/statistics.php
+++ code/statistics.php
@@ -9,7 +9,16 @@
 <body>
     <?php include_once "header.php"; ?>
 
-    
+    <h2 style="left:47%">Statistics for Golf Events</h2>
+    <h3>Events' Popularity</h3>
+    <div style="float:left">
+    </div>
+
+    <h2 style="left:47%">Player statistics</h2>
+    <div style="float:left">
+        <h3>Players with High Win Rates</h3>
+        <h3>Players by Continent</h3>
+    </div>
 
     <?php include_once "footer.php"; ?>
 </body>
