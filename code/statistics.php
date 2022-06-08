f8887f4336a939ef6c02ece67c527b202891db78
gryffindor-coder <65118195+gryffindor-coder@users.noreply.github.com>
2022-06-08 02:39:20 +0200

    Add files via upload
    Skeleton for statistics

---
+++ code/statistics.php
@@ -0,0 +1,15 @@
+<!--Wian du Toit u21433748-->
+<?php include_once "config.php";?>
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="css/global.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Statistics</title>
+</head>
+<body>
+    <?php include_once "header.php"; ?>
+
+    
+
+    <?php include_once "footer.php"; ?>
+</body>
